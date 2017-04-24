3f2209ead1eda9a10f84ace7a8368ab527978ba1
delcanovega <delcanovega@gmail.com>
2017-04-24 19:48:16 +0200

    Template for training section

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return "Work in progress"
+
+@app.route("/train")
+def train():
+    return render_template("train.html")
 
 if __name__ == "__main__":
     app.run(debug = True)
