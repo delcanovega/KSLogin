c4db1581307acaf8cc2c29d2bd1cf96b60fad76f
delcanovega <delcanovega@gmail.com>
2017-04-25 21:03:52 +0200

    Initial work on '/train/<username>'

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
+from flask import request
+from time import time
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    # Pequeño 'about'
+    # Pequeno 'about'
     # Accesos a /train y /login
     # Donaciones
     return "Work in progress"
@@ -18,26 +20,43 @@ def train():
 
 @app.route("/train/<username>", methods=["GET", "POST"])
 def train_user(username):
-    if request.method == 'GET':
-        # Formulario que te pida escribir una palabra (o frase)
-        # Iniciar timestamp
     if request.method == 'POST':
+        values = {}
+        values["collected"] = request.form['value']
+        values["asked"] = request.form['asked']
+        values["ellapsed"] = time() - float(request.form['timestamp'])
+        print values["collected"]
+        print values["asked"]
+        print values["ellapsed"]
         # Extraer features
         # Almacenar features en mongodb
-        # El clasificador se creará y entrenará en /login
+        # El clasificador se creara y entrenara en /login
+        return "POST"
+    if request.method == 'GET':
+        values = {}
+        values["asked"] = "El caballo blanco"
+        # Iniciar timestamp
+        values["timestamp"] = time()
+        values["ellapsed"] = 0
+        values["route"] = "/train/" + username
+        # Formulario que te pida escribir una palabra (o frase)
+        return render_template("training_form.html", vs = values)
+
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == 'GET':
-        # Crear el clasificador y entrenarlo con los datos más actualizados
+        # Crear el clasificador y entrenarlo con los datos mas actualizados
         # (extraidos de mongodb)
         # Formulario que pida nombre de usuario y una palabra (o frase)
         # Iniciar timestamp
         # return render_template("login_form.html")
+        return "GET"
     if request.method == 'POST':
         # Comprobar que coincidencia del usuario con mongodb
         # Extraer features de la frase
         # Usar el clasificador para autenticar
+        return "POST"
 
 if __name__ == "__main__":
     app.run(debug = True)
