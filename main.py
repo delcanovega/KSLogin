a1b1a7190c1cecd445922498747ffc202960c4b9
delcanovega <delcanovega@gmail.com>
2017-05-06 13:23:34 +0200

    Classifier added

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from flask import request
 from time import time
 from humbledb import Mongo, Document
 from random import randint
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
 
 app = Flask(__name__)
 
@@ -93,40 +95,64 @@ def compare_input(target, word):
             break
     return mistakes
 
+@app.route("/dump")
+def dump():
+    with Mongo:
+        data = DataDoc.find()
+        ss = ""
+        for d in data:
+            try:
+                ss += d["user"]
+                ss += ","
+                ss += d["asked"]
+                ss += ","
+                ss += d["collected"]
+                ss += ","
+                ss += str(d["ellapsed"])
+                ss += ","
+                ss += str(d["mistakes"])
+                ss += "\n"
+            except Exception as e:
+                pass
+        output = make_response(ss)
+        output.headers["Content-Disposition"] = "attatchment; filename=export.csv"
+        output.headers["Content-type"] = "text/csv"
+    return output
+
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == 'GET':
-        with Mongo:
-            data = DataDoc.find()
-            ss = ""
-            for d in data:
-                try:
-                    ss += d["asked"]
-                    ss += ";"
-                    ss += d["collected"]
-                    ss += ";"
-                    ss += str(d["ellapsed"])
-                    ss += ";"
-                    ss += str(d["mistakes"])
-                    ss += ";"
-                    ss += d["user"]
-                    ss += "\n"
-                except Exception as e:
-                    pass
-            output = make_response(ss)
-            output.headers["Content-Disposition"] = "attatchment; filename=export.csv"
-            output.headers["Content-type"] = "text/csv"
+        location = r'/home/jdelcano/Downloads/export.csv'
+        headers = ["User", "Asked", "Collected", "Ellapsed", "Mistakes"]
+        data = pd.read_csv(location, names=headers)
         # Crear el clasificador y entrenarlo con los datos mas actualizados
         # (extraidos de mongodb)
+        clf = generate_clf(data)
         # Formulario que pida nombre de usuario y una palabra (o frase)
         # Iniciar timestamp
-        # return render_template("login_form.html")
-        return output
+        values = {}
+        values["timestamp"] = time()
+        values["ellapsed"] = 0
+        return render_template("login_form.html")
     if request.method == 'POST':
         # Comprobar que coincidencia del usuario con mongodb
         # Extraer features de la frase
         # Usar el clasificador para autenticar
         return "POST"
+
+def generate_clf(data):
+    data['SPC'] = data.apply (lambda row: get_spc(row),axis=1)
+    feature_cols = ['Mistakes', 'SPC']
+    X = data[feature_cols]
+    y = data['User']
+
+    clf = RandomForestClassifier(n_estimators=50)
+    clf = clf.fit(X, y)
+    return clf
+
+# SPC: seconds per character
+def get_spc(row):
+    return row[3]/len(row[1])
 
 if __name__ == "__main__":
     app.run(debug = True)
