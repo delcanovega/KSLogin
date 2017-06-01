2a339aabbb1d497a4774adb798837ce4455ec2e2
delcanovega <delcanovega@gmail.com>
2017-06-01 18:08:18 +0200

    Client features working, decent result

--- main.py
+++ main.py
@@ -55,45 +55,57 @@ def train_user(username):
     if request.method == 'POST':
         values = DataDoc()
         values["collected"] = request.form['collected']
-        values["asked"] = request.form['asked']
-        values["ellapsed"] = time() - float(request.form['timestamp'])
         if values["collected"] == "DONE":
             return render_template("training_done.html")
+
         # Extraer features
+        values["asked"] = request.form['asked']
+        values["presd_k"] = request.form['presd_k']
+        values["avg_key_dw"] = request.form['avg_key_dw']
+        values["avg_flight"] = request.form['avg_flight']
+        values["uses_mayus"] = request.form['uses_mayus']
+        print values["presd_k"]
+        values["over_keys"] = int(values["presd_k"]) - len(values["asked"])
         values["mistakes"] = compare_input(values["asked"], values["collected"])
         values["user"] = username
+
         # Almacenar features en mongodb
         with Mongo:
             DataDoc.insert(values)
-        # El clasificador se creara y entrenara en /login
+
         # Reiniciar values para seguir entrenando
-        values["timestamp"] = time()
-        values["ellapsed"] = 0
+        #values["presd_k"] = 0
+        #values["avg_key_dw"] = 0
+        #values["avg_flight"] = 0
+        values["uses_mayus"] = 0
         values["route"] = "/train/" + username
         with Mongo:
             js = WordsDoc.find_one(skip=randint(0,WordsDoc.count()))
             values["asked"] = js["value"]
+
         return render_template("training_form.html", vs = values)
+
     if request.method == 'GET':
         values = {}
         with Mongo:
             js = WordsDoc.find_one(skip=randint(0,WordsDoc.count()))
             values["asked"] = js["value"]
-        # Iniciar timestamp
-        values["timestamp"] = time()
-        values["ellapsed"] = 0
+        #values["presd_k"] = 0
+        #values["avg_key_dw"] = 0
+        #values["avg_flight"] = 0
+        values["uses_mayus"] = 0
         values["route"] = "/train/" + username
         # Formulario que te pida escribir una palabra (o frase)
+
         return render_template("training_form.html", vs = values)
 
 def compare_input(target, word):
     mistakes = 0
-    for i in range(0, len(word)):
-        if (i < len(target) and target[i] != word[i]):
+    for i in range(0, min(len(word), len(target))):
+        if (target[i] != word[i]):
             mistakes = mistakes + 1
-        if (i >= len(target)):
-            mistakes = mistakes + (len(word)-i)
-            break
+    mistakes += max(len(word), len(target)) - min(len(word), len(target))
+
     return mistakes
 
 @app.route("/dump")
@@ -109,68 +121,89 @@ def dump():
                 ss += ","
                 ss += d["collected"]
                 ss += ","
-                ss += str(d["ellapsed"])
+                ss += str(d["presd_k"])
+                ss += ","
+                ss += str(d["avg_key_dw"])
+                ss += ","
+                ss += str(d["avg_flight"])
+                ss += ","
+                ss += str(d["uses_mayus"])
+                ss += ","
+                ss += str(d["over_keys"])
                 ss += ","
                 ss += str(d["mistakes"])
                 ss += "\n"
             except Exception as e:
                 pass
+
         output = make_response(ss)
         output.headers["Content-Disposition"] = "attatchment; filename=export.csv"
         output.headers["Content-type"] = "text/csv"
+
     return output
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == 'GET':
         location = r'/home/jdelcano/Downloads/export.csv'
-        headers = ["User", "Asked", "Collected", "Ellapsed", "Mistakes"]
+        headers = ["user", "asked", "collected", "presd_k", "avg_key_dw",
+                   "avg_flight", "uses_mayus", "over_keys", "mistakes"]
         data = pd.read_csv(location, names=headers)
+        data = data.fillna(0)
+
         # Crear el clasificador y entrenarlo con los datos mas actualizados
         # (extraidos de mongodb)
         clf = generate_clf(data)
         joblib.dump(clf, 'rf_clf.pkl')
-        # Formulario que pida nombre de usuario y una palabra (o frase)
-        # Iniciar timestamp
+
         values = {}
-        values["timestamp"] = time()
-        values["ellapsed"] = 0
+        values["presd_k"] = 0
+        values["avg_key_dw"] = 0
+        values["avg_flight"] = 0
+        values["uses_mayus"] = 0
         with Mongo:
-            js = WordsDoc.find_one(skip=randint(0,WordsDoc.count()))
+            js = WordsDoc.find_one(skip=randint(0, WordsDoc.count()))
             values["asked"] = js["value"]
+
+        # Formulario que pida nombre de usuario y una palabra (o frase)
         return render_template("login_form.html", vs = values)
+
     if request.method == 'POST':
-        values = {}
-        values["collected"] = request.form['collected']
-        values["asked"] = request.form['asked']
-        values["ellapsed"] = time() - float(request.form['timestamp'])
-        values["mistakes"] = compare_input(values["asked"], values["collected"])
-        values["user"] = request.form['username']
-        req = np.array([values["mistakes"], values["ellapsed"]/len(values["collected"])])
-        req = req.reshape(1, -1)
+        # Extraer features
+        v = {}
+        v["collected"] = request.form['collected']
+        v["asked"] = request.form['asked']
+        v["presd_k"] = request.form['presd_k']
+        v["avg_key_dw"] = request.form['avg_key_dw']
+        v["avg_flight"] = request.form['avg_flight']
+        v["uses_mayus"] = request.form['uses_mayus']
+        v["over_keys"] = int(v["presd_k"]) - len(v["asked"])
+        v["mistakes"] = compare_input(v["asked"], v["collected"])
+        v["user"] = request.form['username']
+
+        # Usar el clasificador para autenticar
         clf = joblib.load('rf_clf.pkl')
-        if clf.predict(req) == values["user"]:
+
+        req = np.array([v["avg_key_dw"], v["avg_flight"], v["uses_mayus"],
+                        v["over_keys"], v["mistakes"]])
+        req = req.reshape(1, -1)
+
+        # Comprobar que coincidencia del usuario
+        if clf.predict(req) == v["user"]:
             return "Login succesful"
         else:
             return "Access denied"
-        # Comprobar que coincidencia del usuario
-        # Extraer features de la frase
-        # Usar el clasificador para autenticar
         return "POST"
 
 def generate_clf(data):
-    data['SPC'] = data.apply (lambda row: get_spc(row),axis=1)
-    feature_cols = ['Mistakes', 'SPC']
+    feature_cols = ['avg_key_dw', 'avg_flight', 'uses_mayus', 'over_keys', 'mistakes']
     X = data[feature_cols]
-    y = data['User']
+    y = data['user']
 
     clf = RandomForestClassifier(n_estimators=10)
     clf = clf.fit(X, y)
-    return clf
 
-# SPC: seconds per character
-def get_spc(row):
-    return row[3]/len(row[1])
+    return clf
 
 if __name__ == "__main__":
     app.run(debug = True)
--- static/key_features.js
+++ static/key_features.js
@@ -3,26 +3,30 @@ var start_pr;
 var avg_key_dw = 0;
 var start_fl;
 var avg_flight = 0;
+
 $(document).ready(function(){
-    $("input").keydown(function(){
+    $("#q").keydown(function(event){
+        if (event.which == 20) {  // Uses bloq. mayus
+            $("#f4").val(1);
+        }
         start_pr = $.now();
         if (start_fl > 0) {
             var ellapsed_f = $.now() - start_fl;
             avg_flight = (avg_flight * (presd_k-2)) + ellapsed_f;
             avg_flight /= (presd_k - 1);
 
-            $("#f3").text(avg_flight);
+            $("#f3").val(avg_flight);
         }
     });
-    $("input").keyup(function(){
+    $("#q").keyup(function(){
         presd_k += 1;
         var ellapsed_d = $.now() - start_pr;
         if (ellapsed_d < 1500) {
             avg_key_dw = (avg_key_dw * (presd_k-1)) + ellapsed_d;
             avg_key_dw /= presd_k;
 
-            $("#f1").text(presd_k);
-            $("#f2").text(avg_key_dw);
+            $("#f1").val(presd_k);
+            $("#f2").val(avg_key_dw);
 
             start_fl = $.now();
         }
